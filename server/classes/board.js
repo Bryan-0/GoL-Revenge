993f806550584a0b31cdcb87ff2835a4eedd54f5
Juanma Serrano <joimpoke@gmail.com>
2016-06-11 17:37:17 +0200

    Fix board class
    Fix spaces and wrong usage of x,y vars

--- server/classes/board.js
+++ server/classes/board.js
@@ -1,29 +1,31 @@
 'use strict';
 
 class Board {
-    constructor(id, horizontalDim, verticalDim) {
-        this.id = id;
-        this.horizontalDim = horizontalDim;
-        this.verticalDim = verticalDim;
-        this.board = [];
-        for (let i = 0; i < horizontalDim; i++) {
-            for (let j = 0; j < verticalDim; j++) {
-                this.board[i][j] = new Cell(x, y, getRandomType());
-            }
-        }
-    }
+	constructor(id, horizontalDim, verticalDim) {
+		this.id = id;
+		this.horizontalDim = horizontalDim;
+		this.verticalDim = verticalDim;
+		this.board = [];
+		for (let x = 0; x < horizontalDim; x++) {
+			for (let y = 0; y < verticalDim; y++) {
+				this.board[x][y] = new Cell(x, y, getRandomType());
+			}
+		}
+	}
 
-    getRandomType() {
-        let dice = Math.random() * 100;
-        if (dice < 7)
-            return 2;
+	getRandomType() {
+		let dice = Math.random() * 100;
+		if (dice < 7)
+			return 2;
 
-        if (dice < 14)
-            return 3;
+		if (dice < 14)
+			return 3;
 
-        if (dice < 21)
-            return 4;
+		if (dice < 21)
+			return 4;
 
-        return 1;
-    }
+		return 1;
+	}
 }
+
+module.exports = Board;
