b9c25352b640e95575d79d66b7008626a0a2372e
Juanma Serrano <joimpoke@gmail.com>
2016-06-30 08:47:36 +0200

    Fixing style stuff

--- server/app.js
+++ server/app.js
@@ -3,5 +3,6 @@
 // TODO: No globals.
 global.Config = require('./config/config.js');
 global.Users = require('./classes/users.js');
+// Hey, make a server class that accepts config as a parameter. Users... We'll talk later about users.
 require('./server.js');
 console.log('Game server is set up!');
